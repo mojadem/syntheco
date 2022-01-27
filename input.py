d83c048a2d77ef3c2b5765291a835d553672413a
Shawn T. Brown (PSC) <stbrown@gmail.com>
2022-01-27 08:39:07 -0500

    Adding initial yaml based input file capability

---
+++ input.py
@@ -0,0 +1,70 @@
+"""
+input.def
+This is a class to read input files, validate against the Schema
+and provide a dictinary of hte input parameters
+"""
+
+import pprint
+import yaml
+import input_schema
+
+
+class InputParams:
+    """
+    InputParams class
+
+    This class reads an input file for Syntheco and does all of the validation
+    checking.
+    """
+
+
+    def __init__(self, input_file = "input.yml", input_schema_= input_schema._schema):
+        """
+        Instance Constructor
+
+        Arguments:
+            input_file: a string defining an existing yaml file with inputs
+            input_schema_: the validation schema
+
+        Returns:
+            instance of InputParams
+        """
+
+        self.schema = input_schema_
+        self.input_file = input_file
+        self.input_params = self._read_yaml_file()
+
+    def _read_yaml_file(self):
+        """
+        _read_yaml_file
+        private member that reads and validates the input input_yaml
+
+        Arguments:
+            None
+
+        Returns:
+            dictionary of validated input parameters
+        """
+
+        _yml = None
+        try:
+            with open(self.input_file, "rb") as yaml_file:
+                _yml = yaml.load(yaml_file,yaml.Loader)
+            try:
+                self.schema.validate(_yml)
+                return _yml
+            except Exception as err:
+                print("error: {}".format(err))
+        except Exception as err:
+            print("InputParams Error, Unable to read input file {}\n{}".format(self.input_file,err))
+
+    def __str__(self):
+        pp_print = pprint.PrettyPrinter(indent=4)
+        return pp_print.pformat(self.input_params)
+
+"""
+For Testing
+"""
+if __name__ == "__main__":
+    i = InputParams('canada.yaml')
+    print(i)
---
+++ input_schema.py
@@ -0,0 +1,31 @@
+"""
+input_schema
+
+A module to house the input schema for the yaml input files
+"""
+
+import os
+from schema import Schema, And, SchemaError
+
+_schema = Schema({
+    'census_year':And(int),
+    'census_converter':And(str,lambda x: os.path.exists(f'converters/{x}_converter.py'),
+                                                        error = "Invalid census_converter"),
+    'census_input_files': And(dict, {str: lambda x: os.path.exists(x)},
+                              error = "Invald Census Input Files")
+    })
+
+"""
+For Testing
+"""
+if __name__ == "__main__":
+    test_dict = {'census_year':1994,
+                 'census_converter':'canada_census',
+                 'census_input_files':{'profile_data_csv':'file.csv',
+                                       'pums_h': 'file.csv'}
+                }
+    try:
+        assert _schema.validate(test_dict) == test_dict
+    except SchemaError as e:
+        print("Your Input does not conform to the schema")
+        print(f"Error: {e}")
