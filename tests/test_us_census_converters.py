6bbacd3aeaea10f0fb027fed21dd742b837f99f5
matt dembiczak <dembiczakmatt@gmail.com>
2022-06-14 20:10:41 -0400

    refactored paramater checking for mock functions and implemented pums read

--- tests/test_us_census_converters.py
+++ tests/test_us_census_converters.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import pytest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, create_autospec
 import responses
 from requests.exceptions import HTTPError, ConnectionError, Timeout, RequestException
 import json
@@ -9,6 +9,7 @@ from typing import Callable
 
 import census_converters.plugins.us_census_converters as us
 
+FunctionCallParameters = namedtuple("FunctionCallParameters", ["args", "kwargs"])
 FunctionCallMetadata = namedtuple("FunctionCallMetadata", ["params", "return_value"])
 
 
@@ -153,54 +154,167 @@ class TestUSCensusPlugins:
         expected_output: pd.DataFrame = format_df_meta.return_value
         output: pd.DataFrame = read_func(cens_conv_inst)
 
-        us.api_manager.api_call.assert_called_once_with(*api_call_meta.params)
-        us._format_df.assert_called_once_with(*format_df_meta.params)
+        us.api_manager.api_call.assert_called_once_with(
+            *api_call_meta.params.args, **api_call_meta.params.kwargs
+        )
+        us._format_df.assert_called_once_with(
+            *format_df_meta.params.args, **format_df_meta.params.kwargs
+        )
+
         assert output.equals(expected_output)
 
     def test_global_read(self, cens_conv_inst):
-        api_call_meta_params = [
-            "/2020/dec/pl",
-            {
-                "get": "P1_001N,H1_001N",
-                "for": "county:*",
-                "in": "state:10",
-                "key": "key",
-            },
-        ]
-        api_call_meta_return_value = [
+        api_call_params = FunctionCallParameters(
+            [
+                "/2020/dec/pl",
+                {
+                    "get": "P1_001N,H1_001N",
+                    "for": "county:*",
+                    "in": "state:10",
+                    "key": "key",
+                },
+            ],
+            {},
+        )
+        api_call_return_value = [
             ["P1_001N", "H1_001N", "state", "county"],
             ["570719", "233747", "10", "003"],
             ["237378", "142280", "10", "005"],
             ["181851", "72708", "10", "001"],
         ]
-        api_call_meta = FunctionCallMetadata(
-            api_call_meta_params, api_call_meta_return_value
-        )
+        api_call_meta = FunctionCallMetadata(api_call_params, api_call_return_value)
 
-        format_df_meta_params = [
-            api_call_meta.return_value,
-            cens_conv_inst.input_params,
+        format_df_params = FunctionCallParameters(
             [
-                "P1_001N",
-                "H1_001N",
+                api_call_meta.return_value,
+                cens_conv_inst.input_params,
+                [
+                    "P1_001N",
+                    "H1_001N",
+                ],
             ],
-        ]
-        format_df_meta_return_value = pd.DataFrame(
+            {},
+        )
+        format_df_return_value = pd.DataFrame(
             {
                 "FIPS": ["10003", "10005", "10001"],
                 "P1_001N": ["570719", "237378", "181851"],
                 "H1_001N": ["233747", "142280", "72708"],
             }
         ).set_index("FIPS")
-        format_df_meta = FunctionCallMetadata(
-            format_df_meta_params, format_df_meta_return_value
-        )
+        format_df_meta = FunctionCallMetadata(format_df_params, format_df_return_value)
 
         self.read_scaffold(
             cens_conv_inst,
             api_call_meta,
             format_df_meta,
             us.USCensusGlobalPlugin.read_raw_data_into_pandas,
+        )
+
+    def test_summary_read(self, cens_conv_inst):
+        api_call_params = FunctionCallParameters(
+            [
+                "/2020/acs/acs5/profile",
+                {
+                    "get": "DP04_0039E,DP04_0040E,DP04_0041E,DP04_0042E,DP04_0043E,DP04_0044E",
+                    "for": "county:*",
+                    "in": "state:10",
+                    "key": "key",
+                },
+            ],
+            {},
+        )
+        api_call_return_value = [
+            [
+                "DP04_0039E",
+                "DP04_0040E",
+                "DP04_0041E",
+                "DP04_0042E",
+                "DP04_0043E",
+                "DP04_0044E",
+                "state",
+                "county",
+            ],
+            ["4021", "21129", "46047", "88742", "54050", "10253", "10", "003"],
+            ["1109", "3736", "26295", "74145", "28034", "7804", "10", "005"],
+            ["699", "4101", "13745", "36720", "14367", "3441", "10", "001"],
+        ]
+        api_call_meta = FunctionCallMetadata(api_call_params, api_call_return_value)
+
+        format_df_params = FunctionCallParameters(
+            [
+                api_call_meta.return_value,
+                cens_conv_inst.input_params,
+                [
+                    "DP04_0039E",
+                    "DP04_0040E",
+                    "DP04_0041E",
+                    "DP04_0042E",
+                    "DP04_0043E",
+                    "DP04_0044E",
+                ],
+            ],
+            {},
+        )
+        format_df_return_value = pd.DataFrame(
+            {
+                "FIPS": ["10003", "10005", "10001"],
+                "DP04_0039E": ["4021", "1109", "699"],
+                "DP04_0040E": ["21129", "3736", "4101"],
+                "DP04_0041E": ["46047", "26295", "13745"],
+                "DP04_0042E": ["88742", "74145", "36720"],
+                "DP04_0043E": ["54050", "28034", "14367"],
+                "DP04_0044E": ["10253", "7804", "3441"],
+            }
+        ).set_index("FIPS")
+        format_df_meta = FunctionCallMetadata(format_df_params, format_df_return_value)
+
+        self.read_scaffold(
+            cens_conv_inst,
+            api_call_meta,
+            format_df_meta,
+            us.USCensusSummaryPlugin.read_raw_data_into_pandas,
+        )
+
+    def test_pums_read(self, cens_conv_inst):
+        api_call_params = FunctionCallParameters(
+            [
+                "/2020/acs/acs5/pums",
+                {"get": "BDSP", "for": "state:10", "key": "key"},
+            ],
+            {},
+        )
+        api_call_return_value = [
+            ["BDSP", "state"],
+            ["-1", "10"],
+            ["4", "10"],
+            ["3", "10"],
+            ["3", "10"],
+            ["2", "10"],
+            ["1", "10"],
+            ["-1", "10"],
+            ["3", "10"],
+            ["4", "10"],
+        ]
+        api_call_meta = FunctionCallMetadata(api_call_params, api_call_return_value)
+
+        format_df_params = FunctionCallParameters(
+            [api_call_meta.return_value],
+            {"formulate_fips": False},
+        )
+        format_df_return_value = pd.DataFrame(
+            {
+                "BDSP": ["-1", "4", "3", "3", "2", "1", "-1", "3", "4"],
+                "state": ["10" for _ in range(9)],
+            }
+        )
+        format_df_meta = FunctionCallMetadata(format_df_params, format_df_return_value)
+
+        self.read_scaffold(
+            cens_conv_inst,
+            api_call_meta,
+            format_df_meta,
+            us.USCensusPUMSPlugin.read_raw_data_into_pandas,
         )
 
     def test_global_transform(self, cens_conv_inst):
@@ -244,79 +358,16 @@ class TestUSCensusPlugins:
         )
         assert output_matched
 
-    def test_summary_read(self, cens_conv_inst):
-        api_call_meta_params = [
-            "/2020/acs/acs5/profile",
-            {
-                "get": "DP04_0039E,DP04_0040E,DP04_0041E,DP04_0042E,DP04_0043E,DP04_0044E",
-                "for": "county:*",
-                "in": "state:10",
-                "key": "key",
-            },
-        ]
-        api_call_meta_return_value = [
-            [
-                "DP04_0039E",
-                "DP04_0040E",
-                "DP04_0041E",
-                "DP04_0042E",
-                "DP04_0043E",
-                "DP04_0044E",
-                "state",
-                "county",
-            ],
-            ["4021", "21129", "46047", "88742", "54050", "10253", "10", "003"],
-            ["1109", "3736", "26295", "74145", "28034", "7804", "10", "005"],
-            ["699", "4101", "13745", "36720", "14367", "3441", "10", "001"],
-        ]
-        api_call_meta = FunctionCallMetadata(
-            api_call_meta_params, api_call_meta_return_value
-        )
-
-        format_df_meta_params = [
-            api_call_meta.return_value,
-            cens_conv_inst.input_params,
-            [
-                "DP04_0039E",
-                "DP04_0040E",
-                "DP04_0041E",
-                "DP04_0042E",
-                "DP04_0043E",
-                "DP04_0044E",
-            ],
-        ]
-        format_df_meta_return_value = pd.DataFrame(
-            {
-                "FIPS": ["10003", "10005", "10001"],
-                "DP04_0039E": ["4021", "1109", "699"],
-                "DP04_0040E": ["21129", "3736", "4101"],
-                "DP04_0041E": ["46047", "26295", "13745"],
-                "DP04_0042E": ["88742", "74145", "36720"],
-                "DP04_0043E": ["54050", "28034", "14367"],
-                "DP04_0044E": ["10253", "7804", "3441"],
-            }
-        ).set_index("FIPS")
-        format_df_meta = FunctionCallMetadata(
-            format_df_meta_params, format_df_meta_return_value
-        )
-
-        self.read_scaffold(
-            cens_conv_inst,
-            api_call_meta,
-            format_df_meta,
-            us.USCensusSummaryPlugin.read_raw_data_into_pandas,
-        )
-
     def test_summary_transform(self, cens_conv_inst):
         cens_conv_inst.raw_data_df = pd.DataFrame(
             {
-                "FIPS": ["10003", "10005", "10001"],
-                "DP04_0039E": ["4021", "1109", "699"],
-                "DP04_0040E": ["21129", "3736", "4101"],
-                "DP04_0041E": ["46047", "26295", "13745"],
-                "DP04_0042E": ["88742", "74145", "36720"],
-                "DP04_0043E": ["54050", "28034", "14367"],
-                "DP04_0044E": ["10253", "7804", "3441"],
+                "FIPS": ["10003"],
+                "DP04_0039E": ["4021"],
+                "DP04_0040E": ["21129"],
+                "DP04_0041E": ["46047"],
+                "DP04_0042E": ["88742"],
+                "DP04_0043E": ["54050"],
+                "DP04_0044E": ["10253"],
             }
         ).set_index("FIPS")
 
@@ -330,32 +381,8 @@ class TestUSCensusPlugins:
                         "10003",
                         "10003",
                         "10003",
-                        "10005",
-                        "10005",
-                        "10005",
-                        "10005",
-                        "10005",
-                        "10005",
-                        "10001",
-                        "10001",
-                        "10001",
-                        "10001",
-                        "10001",
-                        "10001",
                     ],
                     "BDSP": [
-                        1,
-                        2,
-                        3,
-                        4,
-                        5,
-                        6,
-                        1,
-                        2,
-                        3,
-                        4,
-                        5,
-                        6,
                         1,
                         2,
                         3,
@@ -370,18 +397,6 @@ class TestUSCensusPlugins:
                         88742.0,
                         54050.0,
                         10253.0,
-                        1109.0,
-                        3736.0,
-                        26295.0,
-                        74145.0,
-                        28034.0,
-                        7804.0,
-                        699.0,
-                        4101.0,
-                        13745.0,
-                        36720.0,
-                        14367.0,
-                        3441.0,
                     ],
                 }
             ).set_index("FIPS")
